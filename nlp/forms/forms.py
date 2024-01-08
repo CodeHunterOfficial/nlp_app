f20f636acafd4307a1f06f7d863e3f50cebeef4c
Arabov Mullosharaf <mullosharaf@gmail.com>
2024-01-08 17:33:59 +0300

    Добавлен css.

--- nlp/forms/forms.py
+++ nlp/forms/forms.py
@@ -1,15 +1,21 @@
 ﻿from django import forms
 
 class CalculatorForm(forms.Form):
-    number1 = forms.FloatField(label='Первое число')
-    number2 = forms.FloatField(label='Второе число')
+    input_text = forms.CharField(label='Исходной текст', widget=forms.Textarea)    
     operation = forms.ChoiceField(
-        choices=(
-            ('add', '+'),
-            ('subtract', '-'),
-            ('multiply', '*'),
-            ('divide', '/'),
+        choices=(   
+            # Список операций с ключами на английском и значениями на русском
+            ("Tokenization", "Токенизация"),
+            ("Lemmatization", "Лемматизация"),
+            ("KeywordExtraction", "Выделение ключевых слов"),
+            ("PartOfSpeechTagging", "Определение частей речи"),
+            ("NamedEntityRecognition", "Разметка именованных сущностей"),
+            ("SentimentAnalysis", "Анализ тональности"),
+            ("CoreferenceResolution", "Разрешение кореференции"),
+            ("MachineTranslation", "Машинный перевод"),
+            ("TextGeneration", "Генерация текста")
         ),
-        widget=forms.Select(), # widget=forms.RadioSelect(),
+        widget=forms.Select(),  # Используем выпадающий список для выбора операции
         label='Выберите операцию'
     )
+    output_text = forms.CharField(label='Результат', widget=forms.Textarea, required=False)
--- nlp/views.py
+++ nlp/views.py
@@ -3,26 +3,14 @@ from django.shortcuts import render
 from nlp.forms.forms import CalculatorForm
 
 
-
 def calculator(request):
-    result = None
     if request.method == 'POST':
         form = CalculatorForm(request.POST)
         if form.is_valid():
-            number1 = form.cleaned_data['number1']
-            number2 = form.cleaned_data['number2']
-            operation = form.cleaned_data['operation']
-            if operation == 'add':
-                result = number1 + number2
-            elif operation == 'subtract':
-                result = number1 - number2
-            elif operation == 'multiply':
-                result = number1 * number2
-            elif operation == 'divide':
-                if number2 != 0:
-                    result = number1 / number2
-                else:
-                    result = "Деление на ноль невозможно"
+            input_text = form.cleaned_data['input_text']
+            # Выполнение вычислений или другой обработки входного текста
+            output_text = input_text.upper()  # Пример: преобразование в верхний регистр
+            form = CalculatorForm(initial={'input_text': input_text, 'output_text': output_text})
     else:
         form = CalculatorForm()
-    return render(request, 'calculator.html', {'form': form, 'result': result})
+    return render(request, 'calculator.html', {'form': form})
